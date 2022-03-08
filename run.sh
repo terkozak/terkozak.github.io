e82fc4681a756bcc348c56a367c846a460bacfea
terkozak <101092357+terkozak@users.noreply.github.com>
2022-03-08 08:59:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "111" > /home/ubuntu/1
+echo "111" > /home/ubuntu/222
 #python3 /home/ubuntu/mhddos/start.py udp 127.0.0.1:53 1 60 &
 #bombardier -c 1 -d 60s http://localhost &
 #sudo shutdown -P +2 &
